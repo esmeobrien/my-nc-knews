598a54460048a84b3bc739ef722a321329ca45d3
Esmé O`Brien <esmeobrien@Esmes-MacBook-Pro.local>
2019-03-06 10:52:27 +0000

    endpoints moved

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -2,10 +2,15 @@ const apiRouter = require('express').Router();
 const topicsRouter = require('./topicsRouter');
 const articlesRouter = require('./articlesRouter.js');
 const usersRouter = require('./usersRouter.js');
+const { endpoints } = require('../endpoints');
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/articles', articlesRouter);
 apiRouter.use('/users', usersRouter);
+
+apiRouter.get('/', (req, res) => {
+  res.status(200).send(endpoints);
+});
 
 
 module.exports = apiRouter;
